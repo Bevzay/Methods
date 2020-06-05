278d64963315271f06e3ca8578c545659ae5777b
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-06-05 13:15:44 +1200

    Working on the challenges

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
 		/*
 		Challenge 2:
-			Create a 2nd method called calculate HighScorePosition
+			Create a 2nd method called calculateHighScorePosition
 			It should be sent one argument only, the player score
 			it should return an int
 			the return data should be:
@@ -28,6 +28,18 @@ public class Main {
 			call both methods and display the results of the following:
 			- a score of 1500, 900, 400 and 50
 		 */
+
+		int highScorePosition = calculateHighScorePosition(1500);
+		displayedHighScorePosition("Tim", highScorePosition);
+
+		highScorePosition = calculateHighScorePosition(900);
+		displayedHighScorePosition("Bob", highScorePosition);
+
+		highScorePosition = calculateHighScorePosition(400);
+		displayedHighScorePosition("Percy", highScorePosition);
+
+		highScorePosition = calculateHighScorePosition(50);
+		displayedHighScorePosition("Larry", highScorePosition);
 
     }
 
@@ -43,6 +55,21 @@ public class Main {
 		}
 	}
 
-	public static void displayedHighScorePosition
+	public static void displayedHighScorePosition(String playerName, int position) {
+		System.out.println(playerName + " managed to get into position " + position + " on the highscore table!");
+	}
+
+	public static int calculateHighScorePosition(int playerScore) {
+
+    	if(playerScore > 1000) {
+    		return 1;
+		} else if(playerScore > 500 && playerScore < 1000) {
+    		return 2;
+		} else if(playerScore > 100 && playerScore < 500) {
+    		return 3;
+		} else {
+    		return 4;
+		}
+	}
 
 }
