5234f4030e830bad801005ce947d035373d440cd
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-06-05 12:50:08 +1200

    Set challenges

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,27 @@ public class Main {
     	calculateScore(true, 800, 5, 100);
     	calculateScore(true, 10000, 8, 200);
 
+    	/*
+    	Challenge 1:
+    		Create a method called displayedHighScorePosition
+			it should be a players name as a parameter, and a 2nd parameter as a position in the high score table
+			you should display the players name along with a message like "managed to get into position" and the
+			position they got and a further message "on the high score table"
+    	 */
+
+		/*
+		Challenge 2:
+			It should be sent one argument only, the player score
+			it should return an int
+			the return data should be:
+			- 1 if the score is >1000
+			- 2 if the score is >500 and <1000
+			- 3 if the score is >100 and <500
+			- 4 in all other cases
+			call both methods and display the results of the following:
+			- a score of 1500, 900, 400 and 50
+		 */
+
     }
 
 	public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -17,7 +38,7 @@ public class Main {
 			System.out.println("Your final score was " + finalScore);
 			return finalScore;
 		} else {
-			return -1;
+			return -1; //shows as an error
 		}
 	}
 
